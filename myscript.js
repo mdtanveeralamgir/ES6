92e002ccd3b6cfcf5b8c63b8a673eca180d218df
mdtanveeralamgir <mdtanveralamgir.com>
2020-10-20 00:08:44 -0400

    null undefine NaN

---
+++ myscript.js
@@ -0,0 +1,26 @@
+//null
+/*
+null is an obj
+it's a falsy value
+null == undefine; //true
+null === undefine; //false (value is same but type is deffirent)
+
+*/
+console.log(typeof null);
+
+//undefine
+/*
+a variable that has not been assigned any value yet
+undefine is a global obj
+if a function returns nothing then it will be undefine
+null == undefine; //true
+null === undefine; //false (value is same but type is deffirent)
+*/
+console.log(typeof undefined);
+
+//NaN
+/*
+Not a number
+gets this message when trying to do some arrithmatinc operation on a non numaric value
+*/
+console.log(typeof NaN);
