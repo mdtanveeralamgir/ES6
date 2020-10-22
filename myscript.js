09f48ad0ab64ed8304f47ef8f4f5e6a8997a84c7
mdtanveeralamgir <mdtanveralamgir.com>
2020-10-21 20:34:04 -0400

    ES6 function rewrite with =>

--- myscript.js
+++ myscript.js
@@ -1,9 +1,38 @@
-//object
-let obj = {
-    "key name" : "value", 
-    integer: 123 //key can be inside double colon if there is no space or special character
-};
 
-//access obj
-console.log(obj["key name"]);
-console.log(obj.integer); //if the key is not wrapped by colons
+
+class Counter{
+    
+    constructor(start = 0, wait){
+        this.count = start;
+        this.wait = wait;
+        this.p = document.createElement("p");
+        document.body.appendChild(this.p);
+    }
+
+
+    countIt(){
+        this.count++;
+        this.p.innerHTML = this.count;
+    }
+
+    /*
+    A call back function
+    
+    */
+    start(){
+        setInterval(() =>{
+        this.countIt();       
+        }
+        , this.wait);
+    }
+     
+}
+
+let count1 = new Counter(100, 300);
+
+let count2 = new Counter(10, 200);
+
+count1.start();
+
+count2.start();
+
