c6e8970e11cbecc6264fe005c7ae960c04912c2b
mdtanveeralamgir <mdtanveralamgir.com>
2020-10-26 00:24:33 -0400

    closure

--- myscript.js
+++ myscript.js
@@ -1,57 +1,25 @@
-//Creating promise
+//cloure
 
-let cleanRoam = new Promise((resolve, reject) => {
-	//Code to clean the room
-	let result = false;
-	if(result){
-		resolve('cleaned');//This argument will be passed to the then callback function.
-	} else{
-		reject('is not cleaned'); //This argument will be passed to the catch callback function.
-	}
-});
+//Lexical scoping: variable define outside function scope is available inside the function but variable define inside function is not 
+//available outside function
 
-cleanRoam.then(fromResolved =>{
-	console.log(fromResolved); //fronResolved is the argument passed to the resolved in the promise
-}).catch(fronReject => {
-	console.log(fronReject); //fronResolved is the argument passed to the reject in the promise
-});
+//Simple example
+let value = 3;
+let addNum = function() {
+    let inner = 2; //only available inside this function
+    return value + inner;
+}
+
+//Closure proper example
+
+let addTo = function(passed) { //A function takes a param (int)
+    return (inner) => passed + inner; //returns a function that takes another param and add it with outer param (passed)
+}
+
+let addTwo = addTo(1);
+console.log(addTwo(2)); //Answer is e
 
 /*
-Chain of promises
-clean the room, clear the garbage, get an ice-cream as reward
-*/ 
-
-//Clean the room
-let cleanRoom = function(){
-	return new Promise((resolve, reject) => {
-		resolve('Cleaned the room');
-	});
-}
-
-//Clear the garbage
-let clearGarbage = function(message){
-	return new Promise((resolve, reject) => {
-		resolve(message + ' Cleared the garbage');
-	});
-}
-
-//Get ice-cream
-let getIceCream = function(message){
-	return new Promise((resolve, reject) => {
-		resolve(message + ' Got ice cream');
-	})
-}
-
-//Chain of promise
-cleanRoom()
-.then((msgAfterCleanRoom) => clearGarbage(msgAfterCleanRoom))
-.then((msgAfterClearGarbeg) => getIceCream(msgAfterClearGarbeg))
-.then((finalMessage) => console.log(finalMessage));
-
-//All finished together
-//values param will catch the message (param) for all the promise
-Promise.all([cleanRoom(), clearGarbage(), getIceCream()]).then((values) => console.log('All finished ' + values));
-
-//Any promise to be finished
-//If any of the 3 promises is finished then we will get the result
-Promise.race([cleanRoom(), clearGarbage(), getIceCream()]).then((values) => console.log('One of them is finished ' + values));
+Above when the addTo returns a function the function preserve the value passed through param "passed"
+this is called closure
+*/
