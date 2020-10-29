9b499e0e485bbf7c8e5281fe2e9d7e64a7f1de83
mdtanveeralamgir <mdtanveralamgir.com>
2020-10-28 23:14:55 -0400

    Partial book-library assignment

--- myscript.js
+++ myscript.js
@@ -1,22 +1,28 @@
-//Object consturctor
-//Best way to creating ab obj to reuse variables for different purpose
-//Needs to be instantiate using new keyword.
-//It's always better to return the toString as a string rather than console.log inside the function
-//Source: https://www.theodinproject.com/courses/javascript/lessons/objects-and-object-constructors
+//Assignment: https://www.theodinproject.com/courses/javascript/lessons/library
+//Creating a library using book object
 
 //Task: creating a obj constructor for book objects
 
+
+//Book object
 function Book(title, author, totalPage, hasRed) {
     this.title = title;
     this.author = author;
     this.totalPage = totalPage;
     this.hasRed = hasRed;
 
-    this.getInfo = function() {
-        return `${this.title} by ${this.author}, ${this.totalPage} pages, ${hasRed ? 'read' : 'has not read yet'}`;
-    };
+
 }
 
-let book1 = new Book('Sondha', 'HA', 200, true);
+//Display book object
+Book.prototype.getInfo = function() {
+    return `${this.title} by ${this.author}, ${this.totalPage} pages, ${this.hasRed ? 'read' : 'has not read yet'}`;
+};
 
-console.log(book1.getInfo());
+//Array to store the book
+
+let book1 = new Book('Debdash', 'HA', 200, true);
+// console.log(book1.getInfo());
+localStorage.setItem('book1', book1);
+let lsb = localStorage.getItem('book1');
+console.log(lsb.toString());
