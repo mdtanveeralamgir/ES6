422661ebde642a24035be81450f1ed67477cc0cb
mdtanveeralamgir <mdtanveralamgir.com>
2020-11-04 01:12:31 -0500

    class, bound method, static method/properties, class expression, computed name

--- myscript.js
+++ myscript.js
@@ -1,145 +1,122 @@
-//Book object
-function Book(title, author, totalPage, hasRead) {
-    this.title = title;
-    this.author = author;
-    this.totalPage = totalPage;
-    this.hasRead = hasRead;
+//Class
+/*
+source: https://javascript.info/class
+source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+class and obj constructor are almost same
+difference is: class has constructor, usese strict mode, and class methods are non-enumerable
+*/
 
-
-}
-
-let book1 = new Book('Debdash', 'HA', 200, 'Yes');
-
-//Display book object
-// Book.prototype.getInfo = function() {
-//     return `${this.title} by ${this.author}, ${this.totalPage} pages, ${this.hasRed ? 'read' : 'has not read yet'}`;
-// };
-
-//Adding book obj into local storage
-function addBookToLocalStorage(bookName, book) {
-    localStorage.setItem(bookName, JSON.stringify(book));
-};
-
-//check if book object is already exist in the localStorage
-function isExist(bookName) {
-    let book = localStorage.getItem(bookName);
-    return book != null ? true : false;
-}
-
-//Retreving book obj from localStorage
-let getBookFromLocalStorage = () => {
-    let table = createTableHead();
-    for (let i = 0; i < localStorage.length; i++) {
-        let bookName = 'book' + i;
-        let element = JSON.parse(localStorage.getItem(bookName));
-        createTable(table, element, bookName);
+class User {
+    constructor(name) {
+        this.name = name;
     }
 
-};
-
-//generate table head
-function createTableHead() {
-    let table = document.createElement('table');
-    table.classList.add('u-full-width');
-    let thead = table.createTHead();
-    let row = thead.insertRow();
-    for (let keys in book1) {
-        let th = document.createElement('th');
-        let text = document.createTextNode(keys);
-        th.appendChild(text);
-        row.appendChild(th);
-    }
-    table.appendChild(row);
-    return table;
-}
-
-//Generating table
-function createTable(table, element, attri) {
-
-    let row = table.insertRow();
-    let tableData = document.getElementById('table-data');
-
-    for (let key in element) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(element[key]);
-        cell.appendChild(text);
-    }
-    //add button
-    let removeButton = addButton('Remove', attri);
-    let readUnread = addButton('Read/Unread', attri);
-    row.appendChild(removeButton);
-    row.appendChild(readUnread);
-    tableData.appendChild(table);
-}
-
-function showHideForm() {
-    let form = document.getElementById('form-div');
-    form.style.display = "block";
-    if (form.style.display == "none") {
-        form.style.display = "block";
-    }
-}
-
-function addButton(buttonName, attri) {
-    let button = document.createElement('BUTTON');
-    button.setAttribute('data-value', attri);
-    let text = document.createTextNode(buttonName);
-    button.appendChild(text);
-    if (buttonName == 'Remove') {
-        button.addEventListener('click', removeItem);
-    } else if (buttonName == 'Read/Unread') {
-        button.addEventListener('click', changeRead);
+    SayHi() {
+        alert('Hi ' + this.name);
     }
 
 
-    // button.addEventListener("click", showHideForm);
-    return button;
-
-}
-
-function removeItem() {
-    localStorage.removeItem(this.getAttribute('data-value'));
-    location.reload();
-
-}
-
-function changeRead() {
-    let bookName = this.getAttribute('data-value');
-    let book = JSON.parse(localStorage.getItem(bookName));
-    if (book.hasRead == 'Yes') {
-        let newBook = new Book(book.title, book.author, book.totalPage, 'No');
-        addBookToLocalStorage(bookName, newBook);
-    } else {
-        let newBook = new Book(book.title, book.author, book.totalPage, 'Yes');
-        addBookToLocalStorage(bookName, newBook);
+    // getter setter
+    get name() {
+        return this._name;
     }
-    location.reload();
-}
+    set name(value) {
+        this._name = value;
+    }
 
+    //class fields
+    printName() {
+        console.log(`${this.name}`);
+    }
 
-function addData() {
-    let title = document.getElementById('title');
-    let author = document.getElementById('author');
-    let totalPage = document.getElementById('totalPage');
-    let hasRead = document.getElementById('hasRead');
+    //prompt for name
+    promptName() {
+        this.name = prompt("Your Name?");
+        alert(this.name);
+    }
 
-    if (title.value == '' || author.value == '' || totalPage.value == '') {
-        console.log('invalid input');
-    } else {
-        let bookName = 'book' + localStorage.length;
-        let book = new Book(title.value, author.value, totalPage.value, hasRead.value);
-        addBookToLocalStorage(bookName, book);
-        location.reload();
+    // makng bound method with the class field
+    click() {
+        console.log(this.name);
+    }
+
+    //elegent approch. see line 53 - 60
+    clickElegent = () => {
+        console.log(this.name);
+    }
+
+    //static method and properties
+    //can't be called via class instances
+    static displayName = "User";
+    static printDisplayName() {
+        console.log(User.displayName); //stati method printing
     }
 }
 
+let user = new User('Opel');
+// user.printName();
+// user.name = 'Shamma'; //Using setter
+// console.log(user.name); //Using getter
+// user.promptName();
+// alert(typeof User); //Function
+// user.SayHi();
 
-function run() {
-    // localStorage.clear();
-    // let bookName = 'book' + 0;
-    // addBookToLocalStorage(bookName, book1);
-    getBookFromLocalStorage();
+//This will not work, nothing will show up
+//This is known as "losing this"
+// setTimeout(user.click, 1000);
 
+//One way to solve this
+setTimeout(() => user.click(), 1000);
+setTimeout(user.clickElegent, 1000);
+
+console.log(User.displayName); //static property
+User.printDisplayName(); //calling static method
+//Class expression
+let anotherClass = class {
+        constructor(name) {
+            this.name = name;
+        }
+        print() {
+            console.log(this.name);
+        }
+    }
+    /*
+    let ac = new anotherClass('opel');
+    ac.print();
+    */
+    //Class expression with name
+    //If a class expression has a name then it's visible inside the class only
+
+let cewn = class MyClass {
+    visibility() {
+        console.log(MyClass); //MyClass Name is visible only iside the class
+    }
 }
 
-run();
+// new cewn().visibility(); //Works
+// console.log(MyClass); //error: my class is not defined
+
+
+//return a class
+function returnClass(name) {
+    return class {
+        print() {
+            console.log(name);
+        }
+    }
+}
+/*
+let returnedClass = returnClass('Aayid');
+let vari = new returnedClass();
+vari.print();
+*/
+//Computed names
+class ComputerName {
+    ['say' + 'hi']() {
+        console.log('Say hi');
+    }
+}
+/*
+let cn = new ComputerName();
+cn.sayhi();
+*/
