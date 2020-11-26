d6eab3991f8b1a2ad8eb335d9b39084c4844e0d6
mdtanveeralamgir <mdtanveralamgir.com>
2020-11-25 22:52:33 -0500

    Class

--- myscript.js
+++ myscript.js
@@ -1,122 +1,44 @@
 //Class
 /*
-source: https://javascript.info/class
 source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
-class and obj constructor are almost same
-difference is: class has constructor, usese strict mode, and class methods are non-enumerable
 */
 
-class User {
+//Binding this with prototype and static methods
+//inheritance: extends
+
+class Animal {
     constructor(name) {
         this.name = name;
     }
-
-    SayHi() {
-        alert('Hi ' + this.name);
+    speak() {
+        return this;
     }
-
-
-    // getter setter
-    get name() {
-        return this._name;
+    sound() {
+        console.log(`${this.name} makes sound.`)
     }
-    set name(value) {
-        this._name = value;
-    }
-
-    //class fields
-    printName() {
-        console.log(`${this.name}`);
-    }
-
-    //prompt for name
-    promptName() {
-        this.name = prompt("Your Name?");
-        alert(this.name);
-    }
-
-    // makng bound method with the class field
-    click() {
-        console.log(this.name);
-    }
-
-    //elegent approch. see line 53 - 60
-    clickElegent = () => {
-        console.log(this.name);
-    }
-
-    //static method and properties
-    //can't be called via class instances
-    static displayName = "User";
-    static printDisplayName() {
-        console.log(User.displayName); //stati method printing
+    static eat() {
+        return this;
     }
 }
 
-let user = new User('Opel');
-// user.printName();
-// user.name = 'Shamma'; //Using setter
-// console.log(user.name); //Using getter
-// user.promptName();
-// alert(typeof User); //Function
-// user.SayHi();
-
-//This will not work, nothing will show up
-//This is known as "losing this"
-// setTimeout(user.click, 1000);
-
-//One way to solve this
-setTimeout(() => user.click(), 1000);
-setTimeout(user.clickElegent, 1000);
-
-console.log(User.displayName); //static property
-User.printDisplayName(); //calling static method
-//Class expression
-let anotherClass = class {
-        constructor(name) {
-            this.name = name;
-        }
-        print() {
-            console.log(this.name);
-        }
+class Dog extends Animal {
+    constructor(name) {
+        super(name);
     }
-    /*
-    let ac = new anotherClass('opel');
-    ac.print();
-    */
-    //Class expression with name
-    //If a class expression has a name then it's visible inside the class only
-
-let cewn = class MyClass {
-    visibility() {
-        console.log(MyClass); //MyClass Name is visible only iside the class
+    sound() {
+        console.log(`${this.name} barks.`)
     }
 }
 
-// new cewn().visibility(); //Works
-// console.log(MyClass); //error: my class is not defined
+let animal = new Animal();
+console.log(animal.speak()); //Animal object
+let speak = animal.speak;
+console.log(speak()); //Undefined
 
+//static methods
+console.log(Animal.eat()); //Class Animal
+let eat = Animal.eat;
+console.log(eat()); //Undefined
 
-//return a class
-function returnClass(name) {
-    return class {
-        print() {
-            console.log(name);
-        }
-    }
-}
-/*
-let returnedClass = returnClass('Aayid');
-let vari = new returnedClass();
-vari.print();
-*/
-//Computed names
-class ComputerName {
-    ['say' + 'hi']() {
-        console.log('Say hi');
-    }
-}
-/*
-let cn = new ComputerName();
-cn.sayhi();
-*/
+let dog = new Dog('Rambo');
+dog.sound();
